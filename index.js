c309d44076795eb29677921e72c52195fa86649e
Megan Day <meganeday7@gmail.com>
2021-05-13 10:31:39 -0500

    Updates to index.js

--- index.js
+++ index.js
@@ -1,121 +1,134 @@
-{/* <input id="search-bar" type="text" placeholder="Harry Potter and the Sorcerer's Stone"></input>
-          <button id="search-button">Search</button> */}
-
-        //   <div id = "all-books"></div>
-
-let searchBar = document.querySelector("input#search-bar")
-let searchButton = document.querySelector("button#search-button")
-let bookLibrary= document.querySelector("div#all-books")
-let addBookForm = document.querySelector("form#book-post-form")
-          let addTitle = addBookForm.querySelector("input#title-input")
-          let addAuthor = addBookForm.querySelector("input#author")
-          let addBookImage = addBookForm.querySelector("input#coverphoto")
-          let addGenre = addBookForm.querySelector("input#genre-input")
-          let addPublicationDate = addBookForm.querySelector("input#publishingdate")
-          let addBookButton = addBookForm.querySelector("button#submit")
-
-
-
-// searchButton.addEventListener("submit")
-
-function viewBooks() {
-    fetch("http://localhost:3000/books")
-    .then(res => res.json())
-    .then((allBooks) => {
-        allBooks.forEach((bookObj) => {
-            let bookSpan= document.createElement("span")
-            let bookImage= document.createElement("img")
-                bookImage.src= bookObj.coverPhoto
-                bookImage.style.width = '200px'
-                bookImage.style.height = '300px'
-                bookSpan.append(bookImage)
-            let bookTitle= document.createElement("h4")
-                bookTitle.innerText=bookObj.title
-                bookSpan.append(bookTitle)
-            let bookAuthor= document.createElement("h5")
-                bookAuthor.innerText=bookObj.author
-                bookSpan.append(bookAuthor)
-            let bookGenre= document.createElement("p")
-                bookGenre.innerText=bookObj.genre
-                bookSpan.append(bookGenre)
-            let bookPublishingDate= document.createElement("p")
-                bookPublishingDate.innerText=bookObj.publishingDate
-                bookSpan.append(bookPublishingDate)
-                bookLibrary.append(bookSpan)
-        });
-    })
-}
-
-viewBooks();
-
-
-// fetch("http://localhost:3000/books", {
-//     method: "POST",
-//     headers: {
-//         "Content-type": "Application/json"
-//     },
-//     body: JSON.stringify({addBookImage})
-// }
-//     .then(res => res.json())
-//     .then((bookObj) => {
-//         let newBookObj = {"addTitle":"title", "addAuthor": "author", "addGenre":"genre", "coverPhoto", "addPublicationDate": "publishingDate"};
-// })
-
-function createBook(){
-    fetch("http://localhost:3000/books", {
-    method: "POST",
-    headers: {
-        "Content-type": "Application/json"
-    },
-    body: JSON.stringify(book)
-}
-    .then(res => res.json())
-    .then((bookObj) => {
-        
-        
+document.addEventListener("DOMContentLoaded", () => {
+    let newBookForm = document.querySelector('form')
+    newBookForm.addEventListener('submit', addBook)
+    fetchAll()
 })
+
+// global variables
+
+let bookLibrary = document.querySelector("div#all-books")
+// let searchBar = document.querySelector("input#search-bar")
+// let searchButton = document.querySelector("button#search-button")
+
+//fetch functions
+
+function fetchAll(){
+    fetch("http://localhost:3000/books")
+        .then(res => res.json())
+        .then(book => book.forEach(book => renderBook(book)))
+
+}
+
+function createBook(book){
+    fetch(`http://localhost:3000/books`, {
+        method: "POST",
+        headers: {
+            "Content-type" : "Application/json"
+        },
+        body: JSON.stringify(book)
+    })
+    .then(res => res.json())
+    .then(book => renderBook(book))
+}
+
+// function patchBook(updatedBook, id){
+//     fetch(`http://localhost:3000/books/${id}`, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-type" : "application/json"
+//         },
+//         body: JSON.stringify(updatedBook)
+//     })
+//     .then(res => res.json())
+//     .then(book => {
+//         bookLibrary.innerHTML = ""
+//         renderBook(book)
+//     })
+// }
+
+// function deleteBook(id){
+//     fetch(`http://localhost:3000/books/${id}`, {
+//         method: "DELETE",
+//         headers: {
+//             "Content-Type" : "application/json"
+//         }
+//     })
+//     .then(res => res.json())
+//     .then(() => {
+//         bookLibrary.innerHTML = ""
+//         fetchAll()
+//     })
+// }
+
+//Event Handlers
 
 function addBook(e){
     e.preventDefault()
     const book = {
         title: e.target['title-input'].value,
-        author: e.target.author.value,
+        author: e.target['author-input'].value,
         genre: e.target['genre-input'].value,
-        coverPhoto : e.target.coverphoto.value,
-        publishingDate: e.target.publishingdate.value
-      }
-      createBook(book)
-          console.log(book);
+        coverPhoto: e.target['cover-input'].value,
+        publishingDate: e.target['published-input'].value
+    }
+    createBook(book)
 }
 
-// function renderNewBook() {
-//     let newBook = document.createElement('span')
-//     newBook.innerHTML = `
-//     <img src=${"coverPhoto.value"}/>
-//     <h4>${"title-input.value"}</h4>
-//     <h5>${"author.value"}</h5>
-//     <p>${"genre-input.value"}</p>
-//     <p>${"publishingdate.value"}</p>
-//     `
+// DOM Manipulation
+
+function renderBook(book) {
+    let bookDiv = document.createElement("div")
+    bookDiv.innerHTML = `
+        <div class='bookInfo' id=${book.id}>
+                <img src="${book.coverPhoto}" width="200px" height="300px"/>
+                <h4>${book.title}</h4>
+                <h5>${book.author}</h5>
+                <p>${book.genre}</p>
+                <p>${book.publishingDate}</p>
+                // div id = "edit-form"
+            <button id="edit-button">Edit Info</button>
+            <button id="delete-button">Delete Book</button>
+        </div>
+        `
+    
+    bookLibrary.append(bookDiv)
+    let editButton = bookDiv.querySelector('#edit-button')
+    console.log(editButton)
+   
+    editButton.addEventListener('click', () => {console.log("hello")})
+
+    // const deleteButton = document.querySelector('#delete-button')
+    // deleteButton.addEventListener('click', () => deleteBook(book.id))
+}
+
+function editBook(book){
+    let form = document.querySelector('form')
+    // form.removeEventListener('submit', addBook)
+    // form.addEventListener('submit', (e) => updateBook(e, book))
+    form['title-input'] = "hi!"
+    // form['title-input'].value = book.title
+    // form['author-input'].value = book.author
+    // form['cover-input'].value = book.coverPhoto
+    // form['genre-input'].value = book.genre
+    // form['published-input'].value = book.publishingDate
+
+    // form.submit.value = 'Edit'
+}
+
+// function updateBook(e, book) {
+//     e.preventDefault()
+
+//     const updatedBook = {
+//         title: e.target['title-input'].value,
+//         author: e.target['author-input'].value,
+//         coverPhoto: e.target['cover-input'].value,
+//         genre: e.target['genre-input'].value,
+//         publishingDate: e.target['published-input'].value
+//     }
+//     form.reset()
+//     form.submit.value = 'submit'
+//     form.removeEventListener('submit', (e) => updateBook(e, book))
+//     form.addEventListener('click', addBook)
+
+//     patchBook(updatedBook, book.id)
 // }
-
-let form = document.querySelector('form')
-    form.reset()
-    form.submit.value = 'submit'
-    form.addEventListener('click', addBook)
-
-
-// fetch("http://localhost:3000/books", {
-//     method: "PATCH",
-//     headers: {
-//         "Content-type": "Application/json"
-//     },
-//     body: json.stringify(data)
-//     })
-//     .then(() => r.json())
-//     .then((bookObj) => {
-//         console.log(bookObj)
-//         let newBook = bookObj
-//     .then()
-//     })
-
